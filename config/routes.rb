52e3bd59735b55f327760e7f43478c30917d5c94
Sanchit Gera <gerasanchit@gmail.com>
2015-01-03 02:57:58 -0500

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   get '/posts/pending', to: 'posts#pending'
   get '/users/show', to: 'users#show'
-  get '/users(/:id)', to: 'users#index', as: 'user_index'
   devise_for :users
   resources :posts do
     resources :comments
   end
   get '/about', to: 'pages#about'
+  get '/users(/:id)', to: 'users#index', as: 'user_index'
   root 'posts#index'
 end
