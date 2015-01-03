fdb86571d02d857fe96b5d26ed839673032ddcbe
Sanchit Gera <gerasanchit@gmail.com>
2015-01-03 01:11:41 -0500

    users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
-    @users = User.all
+    @admins = User.with_role :admin
+    @editors = User.with_role :editor
   end
 end
